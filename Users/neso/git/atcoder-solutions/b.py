dba6d1e7c13b5d75b3a16694fe40e93e70dc42a9
nesosuke <nesosuke@gmail.com>
2020-03-21 23:02:58 +0900

    hoge

--- Users/neso/git/atcoder-solutions/b.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-Height, Width = map(int,input().split())
-masu = Height * Width 
-if Height == 1 or Width == 1:
-    print(1)
-elif masu % 2 == 0:
-    print(masu // 2)
-else:
-    print(masu // 2 + 1)
--- Users/neso/git/atcoder/panasonic2020/c.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-a, b, c = map(float, input().split())
-if (a*b)*4 < ((c-a-b))**2:
-    print("Yes")
-else:
-    print("No")
