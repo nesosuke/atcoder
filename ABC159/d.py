a5f576d9e70ad80d925e08e024b144b0f894b069
nesosuke <nesosuke@gmail.com>
2020-03-22 22:38:19 +0900

    Dわからん。

--- ABC159/d.py
+++ ABC159/d.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 N = int(input())
 A = list(input().split())
+SolCnt = 0
 for i in range(N):
     print(i)
-    print(A[0:i])
-    print(A[i+1::])
+    for j in range(N-i+1):
+        if j != i :
+            for p in range(N-j+1):
+                print("i, j, p", A[i],A[j],A[p])
+                #if A[j] == A[p]:
+                #    SolCnt += 1
+            
+#        if A[i] == A[j]:
+
+       
+#        print(A[0:j])
+#        print(A[j+1::])
+    print(SolCnt)
