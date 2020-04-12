406f7e0f929bb8ad3626b65a422ef4a1f4cbc8a8
nesosuke <nesosuke@gmail.com>
2020-04-12 22:02:25 +0900

    Revert "これはだめ"
    This reverts commit 28fe88270a82865102f46790809f29972c0da8e4.

--- ABC162/c.py
+++ ABC162/c.py
@@ -1,37 +1,14 @@
 #!/usr/bin/python3
-import math
 K = int(input())
 sum_gcd = 0
-
-# K以下の素数
-primelist = []
-for i in range(1, K+1):
-    for j in range(2, int(math.sqrt(K)) + 1):
-        if i % j == 0:
-            primelist.append(i)
-
-# a,bの2項で検索 => 候補抽出(gcd_cand)
-gcd_cand = []
+gcd = 0
 for a in range(1, K+1):
     for b in range(1, K+1):
-        if a == b:
-            gcd_cand.append(a)
-        else:
-            for p in range(len(primelist)):
-                if a % p == 0 and b % p == 0:
-                    gcd_cand.append(a)
-
-print(gcd_cand)
-# cとgcd_candで比較
-for c in range(1, K+1):
-    for q in range(len(gcd_cand)):
-        print(c,gcd_cand[q])
-
-        if c == gcd_cand[q]:
-            gcd = c
-        else:
-            for r in range(len(primelist)):
-                if c % r == 0 and gcd_cand[q] % r == 0:
-                    sum_gcd += r
-
+        for c in range(1, K+1):
+            i = 1
+            while i <= min(a,b,c):
+                if a % i == 0 and b % i == 0 and c % i == 0:
+                   gcd = i  
+                i += 1
+            sum_gcd += gcd
 print(sum_gcd)                
