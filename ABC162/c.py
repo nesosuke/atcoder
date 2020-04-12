b0d0df1cbd514fcee9b8575fff78af1d1bbb7e73
nesosuke <nesosuke@gmail.com>
2020-04-12 22:20:29 +0900

    テスト

--- ABC162/c.py
+++ ABC162/c.py
@@ -2,13 +2,16 @@
 K = int(input())
 sum_gcd = 0
 gcd = 0
-for a in range(1, K+1):
-    for b in range(1, K+1):
-        for c in range(1, K+1):
-            i = 1
+for a in range(K+1,1,-1):
+    for b in range(K+1,1,-1):
+        for c in range(K+1,1,-1):
+            i = max(a,b,c)
             while i <= min(a,b,c):
-                if a % i == 0 and b % i == 0 and c % i == 0:
+                if a == b and b ==c:
+                    gcd = a
+                elif a % i == 0 and b % i == 0 and c % i == 0:
                    gcd = i  
-                i += 1
+                   break
+                i -= 1
             sum_gcd += gcd
 print(sum_gcd)                
