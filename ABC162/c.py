b0d3f944aaa7ab73e987ff86cb79fd72bddd65ed
nesosuke <nesosuke@gmail.com>
2020-04-12 23:18:28 +0900

    AC

--- ABC162/c.py
+++ ABC162/c.py
@@ -2,25 +2,13 @@
 import math
 K = int(input())
 sum_gcd = 0
-gcd = 0
-
-primelist = []	
-for i in range(1, K+1):	
-    for j in range(2, int(math.sqrt(K)) + 1):	
-        if i % j == 0:	
-            primelist.append(i)
-
-for a in range(K,0,-1):
-    for b in range(K,0,-1):
-        for c in range(K,0,-1):
-            i = max(a,b,c)
-            while i >= min(a,b,c):
-                if a in primelist or b in primelist or c in primelist:
-                    if a == b and b ==c:
-                        gcd = a
-                elif a % i == 0 and b % i == 0 and c % i == 0:
-                   gcd = i  
-                   break
-                i -= 1
-            sum_gcd += gcd
+for a in range(1,K+1):
+    for b in range(1,a+1):
+        for c in range(1,b+1):
+            if a == b and b == c :
+                sum_gcd += math.gcd(math.gcd(a,b),c)
+            elif a == b or b == c or c == a:
+                sum_gcd += 3 * math.gcd(math.gcd(a,b),c)
+            else:
+                sum_gcd += 6 * math.gcd(math.gcd(a,b),c)
 print(sum_gcd)
