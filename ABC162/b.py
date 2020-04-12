d7b7e8c3642d338a550deda258cc85facac6083a
nesosuke <nesosuke@gmail.com>
2020-04-12 22:06:10 +0900

    range範囲テスト

--- ABC162/b.py
+++ ABC162/b.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 N = int(input())
 wa = 0
-for i in range(N):
-    if i % 3 != 0 and i % 5 != 0:
+for i in range(1,N+1):
+    if i % 3 != 0 and i % 5 != 0 and i % 15 != 0:
         wa += i
 print(wa)
