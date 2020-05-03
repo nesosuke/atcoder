fee6c5d15128247aa5a34f33e4a16a49754111d1
nesosuke <nesosuke@gmail.com>
2020-05-03 22:40:00 +0900

    わからない

---
+++ abc166/c.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+n, m = map(int,input().split())
+h = list(input().split())
+h_comp = h
+## path
+for i in range(m):
+    a,b = map(int,input().split())
+    if h[a-1] <= h[b-1]:
+            h_comp[a-1] = "Bad"
+    elif h[b-1] >= h[a-1]:
+            h_comp[b-1] = "Bad"
+
+count = 0
+for i in range(n):
+    if h_comp[i] != "Bad":
+        count += 1
+
+print(h_comp)
+print(count)
